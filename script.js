80e9feabbafc697bf6ac73ba3bfbe6e412289c48
Hemanth-Kumar007 <121893071+Hemanth-Kumar007@users.noreply.github.com>
2023-11-13 23:12:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function stringChop(str, size) {
   // your code here
+	let ans = [];
+	let i=0;
+	while(i<str.length-1){
+		ans.push(str.slice(i,i+2));
+		i = i+2;
+	}
 }
 
 // Do not change the code below
